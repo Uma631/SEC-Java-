58b6f8f3b5758a0ca429213f300b42a771280207
Uma HM <p7027101@gmail.com>
2025-12-02 14:10:51 +0530

    Create CountVowels.java

---
+++ CountVowels.java
@@ -0,0 +1,12 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = “Java Programming";
+        int count = 0;
+        for (char ch : str.toLowerCase().toCharArray()) {
+            if ("aeiou".indexOf(ch) != -1) {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
